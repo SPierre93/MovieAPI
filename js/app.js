e5b3198efab4fb44c5ad8ed2c97d1af718ab1987
ShireenPierre <ttpolitricks@gmail.com>
2020-07-08 04:44:41 -0400

    First commit; Page 2 started

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const page1 = document.querySelector("#homePageContainer");
+const page2 = document.querySelector("#movieDisplayPageContainer");
+const page3 = document.querySelector("#movieDetailsPageContainer");
+
+/*
+MAKE PAGES DISAPPEAR
+page1.style.display="none";
+page2.style.display="none";
+page3.style.display="none";
+*/
